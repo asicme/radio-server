35ffc7e1332debe7282406acd4d9d61c4f8cf61f
asicme <alexisgavidia21@gmail.com>
2026-01-19 14:35:23 -0400

    Refactor main.go to simplify server functionality

--- main.go
+++ main.go
@@ -1,40 +1,15 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"net/http"
-	"github.com/quic-go/webtransport-go"
 )
 
 func main() {
-	server := webtransport.Server{
-		CheckOrigin: func(r *http.Request) bool { return true },
-	}
-
-	http.HandleFunc("/radio", func(w http.ResponseWriter, r *http.Request) {
-		session, err := server.Upgrade(w, r)
-		if err != nil {
-			return
-		}
-		
-		for {
-			stream, err := session.AcceptUniStream(context.Background())
-			if err != nil {
-				break
-			}
-			go func(s webtransport.ReceiveStream) {
-				buf := make([]byte, 4096)
-				for {
-					_, err := s.Read(buf)
-					if err != nil {
-						break
-					}
-				}
-			}(stream)
-		}
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Servidor de Radio Online OK")
 	})
 
-	fmt.Println("Servidor en puerto 8080")
+	fmt.Println("Escuchando en el 8080...")
 	http.ListenAndServe(":8080", nil)
 }
